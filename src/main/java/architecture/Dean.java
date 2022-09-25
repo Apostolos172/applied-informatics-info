c89705e5c90b5c33925f5abfeb4cccad9ccf43d5
Apostolos172 <apostolos.vad@gmail.com>
2022-09-25 22:33:11 +0300

    complete types of professor with the necessary overriding and some constuctors and definition of the class

--- src/main/java/architecture/Dean.java
+++ src/main/java/architecture/Dean.java
@@ -2,28 +2,30 @@ package architecture;
 
 import java.util.ArrayList;
 
-public class Dean extends Professor {
+public class Dean extends NormalProfessor {
 	
-	private String type;
-
 	public Dean(String first_name, String last_name, String phone) {
 		super(first_name, last_name, phone);
 		// TODO Auto-generated constructor stub
-		this.type = "Κοσμήτορας";
+		super.setType("Κοσμήτορας");
 	}
 
 	public Dean(String first_name, String last_name, String phone, String email, ArrayList<Course> courses) {
 		super(first_name, last_name, phone, email, courses);
 		// TODO Auto-generated constructor stub
-		this.type = "Κοσμήτορας";
+		super.setType("Κοσμήτορας");
 	}
 
+	public Dean(String first_name, String last_name, String phone, String email) {
+		super(first_name, last_name, phone, email);
+		// TODO Auto-generated constructor stub
+		super.setType("Κοσμήτορας");
+	}
+	
 	@Override
 	public String getInfo() {
 		// TODO Auto-generated method stub
-		return this.type;
+		return super.getType();
 	}
-	
-	
 
 }
