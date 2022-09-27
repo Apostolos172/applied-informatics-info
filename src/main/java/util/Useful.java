b3998c8a43e16f1f543e0fff1f672d34b212f2a8
Apostolos172 <apostolos.vad@gmail.com>
2022-09-27 08:47:27 +0300

    two new fields for the professor class in order to achieve the saving of the image existence

--- src/main/java/util/Useful.java
+++ src/main/java/util/Useful.java
@@ -1,5 +1,9 @@
 package util;
 
+import java.io.File;
+
+import javax.servlet.http.HttpServletRequest;
+
 import architecture.Assistant;
 import architecture.Associate;
 import architecture.Dean;
@@ -10,6 +14,21 @@ public class Useful {
 
 	public Useful() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	public static boolean photoFileExists(String photoFileName, HttpServletRequest request, StringBuilder suffix) {
+		String path = request.getServletContext().getRealPath( "/images/professors/"+photoFileName+".jpg" );
+		File f = new File(path);
+		boolean exists = f.exists();
+		suffix.append(".jpg");
+		if(!exists) {
+			path = request.getServletContext().getRealPath( "/images/professors/"+photoFileName+".png" );
+			f = new File(path);
+			exists = f.exists();
+			suffix.delete(0, suffix.length());
+			suffix.append(".png");
+		}
+		return exists;
 	}
 	
 	public static String getProfessorTypeAlias(String cat) {
